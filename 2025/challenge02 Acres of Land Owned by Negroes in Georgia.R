1c3973c11766548c8ba3aad4df87586f33eb2c88
drjohnrussell <russellj@gmail.com>
2025-02-03 21:08:24 -0500

    drafting Challenge 02

--- 2025/challenge02 Acres of Land Owned by Negroes in Georgia.R
+++ 2025/challenge02 Acres of Land Owned by Negroes in Georgia.R
@@ -5,22 +5,19 @@ library(ggimage)
 font_add_google("Play", family = "dubois")
 showtext_auto()
 
-data <- read_csv("https://github.com/ajstarks/dubois-data-portraits/raw/refs/heads/master/challenge/2025/challenge01/data.csv") |> 
-  mutate(image2="2025/images/rawpixel.jpg",
-         position=`Land Value (Dollars)`^(1/3))
+data <- read_csv("https://github.com/ajstarks/dubois-data-portraits/raw/refs/heads/master/challenge/2025/challenge02/data.csv") |> 
+  mutate(Date=as.factor(Date),
+         color=case_when(Date %in% c("1874","1899") ~ "black",
+                         .default="transparent"))
 
-library(ggimg)
-p <- ggplot(data, mapping=aes(img=image2)) +
-  geom_rect_img(mapping=aes(xmin=0-position/5,
-                            xmax=0+position/5,
-                            ymin=0,
-                            ymax=200)) +
-  geom_text(mapping=aes(label=paste0("$",`Land Value (Dollars)`)),y=75,x=0,size=3,family="dubois") +
-  scale_y_continuous(limits=c(0,200)) +
-  scale_x_continuous(limits=c(-100,100)) +
+p <- ggplot(data,mapping=aes(x=fct_rev(Date))) +
+  geom_col(mapping=aes(y=Land),fill="#cf334e",width=.7) +
+  geom_text(mapping=aes(label=Land,y=Land,color=color),position=position_stack(vjust=.5),size=5,show.legend=FALSE) +
+  scale_color_manual(values=c("black","#cf334e"))+
+  coord_flip() +
   theme_void() +
-  labs(title=str_to_upper("value of land owned by georgia negroes")) +
   theme(text = element_text(family = "dubois", size = 20, lineheight = .5),
+        axis.text.y=element_text(size=15,color="#636363"),
         panel.background = element_rect(fill = NA, color = NA),
         panel.ontop = TRUE,
         panel.grid.major.x = element_blank(),
@@ -36,6 +33,4 @@ p <- ggplot(data, mapping=aes(img=image2)) +
         plot.caption = element_text(size = 10), 
         plot.caption.position = "plot",
         strip.text = element_text(size = 15, hjust = 0.5)) +
-  facet_wrap(~Year,ncol=1,scales="free",strip.position="bottom")
-
-ggsave(plot=p,filename="2025/challenge01.pdf",width=5.5,height=8,units="in",dpi=600,bg="#E6D4C3")
+  labs(title=str_to_upper("acres of land owned by negroes \n in georgia."))

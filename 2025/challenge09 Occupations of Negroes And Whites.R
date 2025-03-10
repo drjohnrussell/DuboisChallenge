9fa92473031861c88f69470c4687db7d3945883d
drjohnrussell <russellj@gmail.com>
2025-03-09 20:37:56 -0400

    Reshaped folders and finished challenge 9

--- 2025/challenge09 Occupations of Negroes And Whites.R
+++ 2025/challenge09 Occupations of Negroes And Whites.R
@@ -16,11 +16,11 @@ data <- read_csv("https://github.com/ajstarks/dubois-data-portraits/raw/refs/hea
 
 circles <- data.frame(Occupation=levels(data$Occupation),
                       x=c(-.7,.7,-.7,.7,.7),
-                      y=c(.15,.3,-.15,-.3,0))
+                      y=c(.15,.25,-.15,-.25,0))
 
 text <- data.frame(Occupation=levels(data$Occupation),
-                   x=c(-.6,.6,-.6,.6,.6),
-                   y=c(.15,.3,-.15,-.3,0),
+                   x=c(-.63,.63,-.63,.63,.63),
+                   y=c(.15,.25,-.15,-.25,0),
                    hjust=c(0,1,0,1,1))
 
 
@@ -44,23 +44,24 @@ data2 <- data |>
 p <- data2 |> 
   ggplot() +
   geom_arc_bar(aes(x0=0, y0=0, r0=0, r=1, start=start, end=end, fill=Occupation), color="black",linewidth=.1) +
-  geom_text(aes(x=textx, y=texty, label=paste0(Percentage,"%")), family="dubois", size=5) +
-  geom_mark_circle(data=circles,aes(x=x, y=y, fill=Occupation), expand=unit(.2,"cm"),alpha=1, color="black") +
-  geom_text(data=text,aes(x=x, y=y, label=str_wrap(Occupation,20), hjust=hjust),vjust=0.5,lineheight=.5, family="dubois", size=5) +
+  geom_text(aes(x=textx, y=texty, label=paste0(Percentage,"%")), family="dubois", size=10) +
+  geom_mark_circle(data=circles,aes(x=x, y=y, fill=Occupation), expand=unit(1.5,"cm"),alpha=1, color="black") +
+  geom_text(data=text,aes(x=x, y=y, label=str_wrap(Occupation,20), hjust=hjust),vjust=0.5,lineheight=.8, family="dubois", size=11) +
   theme_void() +
   coord_fixed() +
   scale_fill_manual(values=c("#dc143c", "#ffd700", "#4682b4", "#d2b48c", "#654321")) +
   guides(fill="none") +
-  annotate("text", x=0, y=1.05, label="NEGROES.",alpha=.8,family="dubois") +
-  annotate("text", x=0, y=-1.05, label="WHITES", alpha=.8,family="dubois") +
+  annotate("text", x=0, y=1.05, label="NEGROES.",alpha=.8,family="dubois",size=14) +
+  annotate("text", x=0, y=-1.05, label="WHITES", alpha=.8,family="dubois",size=14) +
   labs(title=str_to_upper("occupations of negroes and whites in georgia.")) +
   theme(legend.position="none",
         text = element_text(family = "dubois", size = 20, lineheight = .5),
         panel.background = element_rect(fill = NA, color = NA),
         panel.ontop = TRUE,
-        plot.title = element_text(size = 45, hjust=.5,lineheight=.8,margin=margin(0.2,0,1,0,"cm")),
+        plot.title = element_text(size = 48, hjust=.5,lineheight=.8,margin=margin(0.2,0,1,0,"cm")),
         plot.title.position = "plot",
         plot.background = element_rect(fill = "#E6D4C3", color = NA),
         plot.margin = margin(1, 1, 0.1, 1, "cm"))
 
 ggsave(p, filename = "2025/challenge09.pdf", width=22,height=28,units="in",dpi=600,bg="#E6D4C3")
+
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<h1 align="center">
-DuBois Challenge</h1>
+# **DuBois Challenge**
 
 The DuBois Challenge is part of a celebration of the data visualization legacy of W.E.B DuBois which attempts to recreate the visualizations from the 1900 Paris Exposition using modern tools. Check out the challenge [here](https://github.com/ajstarks/dubois-data-portraits/blob/master/challenge/README.md).

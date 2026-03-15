8540a81a4b25895914b3030d8e141676f45b4c28
drjohnrussell <russellj@gmail.com>
2026-03-15 06:21:49 -0400

    Challenge 05 fixes

--- 2026/challenge 05 Migration of Black People.R
+++ 2026/challenge 05 Migration of Black People.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(sf)
 library(rnaturalearth)
+library(showtext)
+library(ggview)
 
 brown <- "#654321"
 tan <- "#d2b48c"
@@ -38,35 +40,82 @@ us_states <- us_states |>
 
 ## we can then create a data frame that has the centroids of georgia and the centroids of each state, and then we can use that to create the lines
 # Get Georgia centroid as a single point
-georgia_centroid <- us_states |>
-  filter(name == "Georgia") |>
-  st_centroid() |>
-  st_coordinates() |>
-  as_tibble() |>
-  rename(georgia_lon = X, georgia_lat = Y)
+georgia_centroid <- us_states$centroid[us_states$name == "Georgia"]
 
-# Get all state centroids as one row per state
-state_centroids <- us_states |>
-  st_centroid() |>
-  st_coordinates() |>
-  as_tibble() |>
-  rename(state_lon = X, state_lat = Y)
-
-## create a line going from the centroid of georgia to the 
-## state centroids, but trimmed to the geometry of the state
-lines <- state_centroids |>
-  bind_cols(georgia_centroid) |>
+lines <- us_states |>
+  filter(name != "Georgia") |>
+  select(name, postal, centroid) |>
+  mutate(
+    line = st_sfc(
+      mapply(
+        function(a, b) st_linestring(matrix(c(st_coordinates(a), st_coordinates(b)), nrow = 2, byrow = TRUE)),
+        centroid,
+        rep(georgia_centroid, n()),
+        SIMPLIFY = FALSE
+      ),
+      crs = st_crs(us_states)
+    )
+  ) |>
+  st_set_geometry("line") |> 
+  ## shorten the linestring to only that part that is within the state
   rowwise() |>
-  mutate(line = st_sfc(st_linestring(matrix(c(georgia_lon, georgia_lat, state_lon, state_lat), ncol = 2)))) |>
-  ungroup() |>
-  st_as_sf(crs = st_crs(us_states))
+  mutate(line = st_intersection(geometry, line)) |>
+  ungroup()
 
-## now we can trim the lines to the geometry of the states
-trimmed_lines <- st_intersection(lines, us_states)
-### this did not work, there are 0 rows
-## we can try to do this manually by creating a line from the centroid of georgia to the centroid of each state, and then trimming it to the geometry of the state
-trimmed_lines_manual <- lines |>
-  rowwise() |>
-  mutate(trimmed_line = st_intersection(line, us_states$geometry[which(us_states$postal == state_centroids$postal)])) |>
-  ungroup() |>
-  st_as_sf(crs = st_crs(us_states))
+textpresent <- presentlocation |> 
+  left_join(us_states, by = c("State" = "postal")) |> 
+  st_as_sf() |> 
+  st_set_geometry("centroid")
+mappresent <- us_states |> 
+  left_join(presentlocation, by = c("postal" = "State"))
+mapbirth <- us_states |> 
+  left_join(birthlocation, by = c("postal" = "State"))
+
+textbirth <- birthlocation |>
+  filter(!is.na(Birthplace)) |> 
+  left_join(us_states, by = c("State" = "postal")) |> 
+  st_as_sf() |> 
+  st_set_geometry("centroid")
+
+
+## now we can plot the map
+topmap <- ggplot() +
+  geom_sf(data = mappresent, aes(fill = color), color = "black", show.legend=FALSE, alpha=.8) +
+  scale_fill_manual(values = c(background, "black", lightblue, brown, crimson, gold, green, pink, tan)) +
+  geom_sf(data = lines, color = "darkgrey", size = 2, alpha=.7, arrow = arrow(length = unit(0.8, "cm"), ends="first", type = "open")) +
+  geom_sf_text(data = textpresent, aes(label = `Present Location`, color=ifelse(name == "Georgia", "white", "black")), size = 25, family = "dubois", vjust= 1.5, show.legend=FALSE) +
+  scale_color_manual(values = c("black", "white")) +
+  ## set to orthographic projection centered on missouri
+  coord_sf(crs = sf::st_crs("+proj=ortho +lat_0=39 +lon_0=-98")) +
+  theme_void() +
+  labs(caption = "PRESENT DWELLING PLACE OF BLACK PEOPLE BORN IN GEORGIA") +
+  theme(panel.background = element_rect(fill = background, color = background),
+        plot.background = element_rect(fill = background, color = background),
+## place title underneat plot
+        plot.caption = element_text(family = "dubois", size = 150, hjust = 0.5, vjust = 0.5),
+        plot.margin = margin(t = 10, r = 20, b = 10, l = 20))
+
+bottommap <- ggplot() +
+  geom_sf(data = mapbirth, aes(fill = color), color = "black", show.legend=FALSE, alpha=.8) +
+  scale_fill_manual(values = c(background, "black", lightblue, brown, crimson, gold, green, pink, tan)) +
+  geom_sf_text(data = textbirth, aes(label = `Birthplace`, color=ifelse(name == "Georgia", "white", "black")), size = 25, family = "dubois", vjust= 2, show.legend=FALSE) +
+  scale_color_manual(values = c("black", "white")) +
+  geom_sf(data = lines |> filter(name %in% textbirth$name), color = "darkgrey", size = 2, alpha=.7, arrow = arrow(length = unit(0.8, "cm"), ends="last", type = "open")) +
+  ## set to orthographic projection centered on missouri
+  coord_sf(crs = sf::st_crs("+proj=ortho +lat_0=39 +lon_0=-98")) +
+  theme_void() +
+  labs(caption = "BIRTH PLACE OF BLACK PEOPLE NOW RESIDENT IN GEORGIA") +
+  theme(panel.background = element_rect(fill = background, color = background),
+        plot.background = element_rect(fill = background, color = background),
+        plot.caption = element_text(family = "dubois", size = 150, hjust = 0.5, vjust = 0.5),
+        plot.margin = margin(t = 10, r = 20, b = 10, l = 20))
+
+library(patchwork)
+
+final <- topmap / bottommap + plot_annotation(title = "MIGRATION OF BLACK PEOPLE. \n 1890", 
+                                              theme = theme(plot.title = element_text(family = "dubois", size = 200, hjust = 0.5, vjust = 0, lineheight=.25),
+                                                            plot.background = element_rect(fill = background, color = background)))
+
+final + canvas(width=22, height=28, units="in", bg=background)
+
+ggsave("2026/final/challenge05.png", final, width = 22, height = 28, units = "in", bg = background)
